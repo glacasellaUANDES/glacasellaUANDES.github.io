e641a85475a7b285f9d3a888fb2fec7b8c5c1aeb
glacasellaUANDES <glacasella@miuandes.cl>
2020-02-19 16:56:26 -0300

    Working on navbar

--- js/main.js
+++ js/main.js
@@ -19,10 +19,36 @@ $(function() {
     */
 
 
-    // Menu responsive
-    $('.imagen-firma').on('click', function() {
-        $('.navegacion-izquierda').slideToggle();
-    });
+    (function() {
+
+        var nav = $('nav'),
+            menu = $('nav h1'),
+            main = $('main'),
+            open = false,
+            hover = false;
+
+        menu.on('click', function() {
+            open = !open ? true : false;
+            nav.toggleClass('menu-active');
+            main.toggleClass('menu-active');
+            nav.removeClass('menu-hover');
+            main.removeClass('menu-hover');
+            console.log(open);
+        });
+        menu.hover(
+            function() {
+                if (!open) {
+                    nav.addClass('menu-hover');
+                    main.addClass('menu-hover');
+                }
+            },
+            function() {
+                nav.removeClass('menu-hover');
+                main.removeClass('menu-hover');
+            }
+        );
+
+    })();
 
 
 });
