272eb0131c429ebde22c7ee03905f30f7ea03c94
glacasellaUANDES <glacasella@miuandes.cl>
2020-02-19 18:26:30 -0300

    Avanzando

--- js/main.js
+++ js/main.js
@@ -1,54 +1,59 @@
-$(function() {
+jQuery(document).ready(function($){
+	//open navigation clicking the menu icon
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		toggleNav(true);
+	});
+	//close the navigation
+	$('.cd-close-nav, .cd-overlay').on('click', function(event){
+		event.preventDefault();
+		toggleNav(false);
+	});
+	//select a new section
+	$('.cd-nav li').on('click', function(event){
+		event.preventDefault();
+		var target = $(this),
+			//detect which section user has chosen
+			sectionTarget = target.data('menu');
+		if( !target.hasClass('cd-selected') ) {
+			//if user has selected a section different from the one alredy visible
+			//update the navigation -> assign the .cd-selected class to the selected item
+			target.addClass('cd-selected').siblings('.cd-selected').removeClass('cd-selected');
+			//load the new section
+			loadNewContent(sectionTarget);
+		} else {
+			// otherwise close navigation
+			toggleNav(false);
+		}
+	});
 
-    /*
-    // Menu fijo
-    // Evento que va a estar escuchando en todo momento la distancia 
-    // a la que se ha hecho scroll en la pagina, medida desde arriba
-    var windowHeight = $(window).height(); // Altura de la ventana del nav
-    var barraAltura = $('.barra').innerHeight(); // Altura de la barra de navegacion
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll > windowHeight) {
-            $('.barra').addClass('fixed');
-            $('body').css({ 'margin-top': barraAltura + 'px' });
-        } else {
-            $('.barra').removeClass('fixed');
-            $('body').css({ 'margin-top': '0px' });
-        }
-    });
-    */
+	function toggleNav(bool) {
+		$('.cd-nav-container, .cd-overlay').toggleClass('is-visible', bool);
+		$('main').toggleClass('scale-down', bool);
+	}
 
+	function loadNewContent(newSection) {
+		//create a new section element and insert it into the DOM
+		var section = $('<section class="cd-section '+newSection+'"></section>').appendTo($('main'));
+		//load the new content from the proper html file
+		section.load(newSection+'.html .cd-section > *', function(event){
+			//add the .cd-selected to the new section element -> it will cover the old one
+			section.addClass('cd-selected').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//close navigation
+				toggleNav(false);
+			});
+			section.prev('.cd-selected').removeClass('cd-selected');
+		});
 
-    (function() {
+		$('main').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			//once the navigation is closed, remove the old section from the DOM
+			section.prev('.cd-section').remove();
+		});
 
-        var nav = $('nav'),
-            menu = $('nav h1'),
-            main = $('main'),
-            open = false,
-            hover = false;
-
-        menu.on('click', function() {
-            open = !open ? true : false;
-            nav.toggleClass('menu-active');
-            main.toggleClass('menu-active');
-            nav.removeClass('menu-hover');
-            main.removeClass('menu-hover');
-            console.log(open);
-        });
-        menu.hover(
-            function() {
-                if (!open) {
-                    nav.addClass('menu-hover');
-                    main.addClass('menu-hover');
-                }
-            },
-            function() {
-                nav.removeClass('menu-hover');
-                main.removeClass('menu-hover');
-            }
-        );
-
-    })();
-
-
+		if( $('.no-csstransitions').length > 0 ) {
+			//if browser doesn't support transitions - don't wait but close navigation and remove old item
+			toggleNav(false);
+			section.prev('.cd-section').remove();
+		}
+	}
 });
